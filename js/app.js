3b7d3fff335693034523fe22a479810090f1bf87
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-13 18:37:25 +0100

    ADD: bonus

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
 
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
 // collego bottone con js quando clicco si vedono tutte le celle
 let btnStart = document.querySelector('.btn-start'); // dom element
 console.log(btnStart)
@@ -42,8 +49,4 @@ for(let i = 1; i < interation; i++){
 
 }
 
-
-
-
-// creo celle della grigli all'evento clic sul btnStart
 
